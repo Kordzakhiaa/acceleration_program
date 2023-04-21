88d7ba9b2a00b60df5c32694ee2345180844f9b0
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-22 00:39:45 +0400

    add: registration and login api views. Register view is with its own serializer and login view is child class of TokenObtainPairView. We extend it for future purposes.

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from apps.accounts.serializers import (
+    UserRegistrationSerializer,
+)
+
+
+class RegistrationAPIView(GenericAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def post(self, request: "Request", *args, **kwargs) -> "Response":
+        """Post method that handles user registration from api endpoint"""
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class LoginAPIView(TokenObtainPairView):
+    pass
