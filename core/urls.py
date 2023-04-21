4e23e9d530c62a60ca4310f5d7d6ed166a35c836
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-22 00:41:30 +0400

    add: refresh token view and project app accounts root url

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # PROJECT APPS ENDPOINTS
+    path('api/auth/', include('apps.accounts.urls')),
+
+    # THIRD-PARTY APPS ENDPOINTS
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
