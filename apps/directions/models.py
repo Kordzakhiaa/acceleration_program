ecd58ab647f620d8d09961e36771c0be1e4c70f3
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-23 21:42:31 +0400

    create: Direction model

--- apps/directions/models.py
+++ apps/directions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Direction(models.Model):
+    title = models.CharField(max_length=150)
+    number_of_stages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
