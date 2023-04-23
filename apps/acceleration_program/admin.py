b38f07d48b8c616898b718c404be910c5ab3a7d4
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-23 22:40:22 +0400

    add: newly added models to django admin interface

--- apps/acceleration_program/admin.py
+++ apps/acceleration_program/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.acceleration_program.models import (
+    AccelerationProgram,
+    Stage,
+    JoinProgram,
+    Applicants,
+    StageType,
+    OrderedStages,
+    ApplicantResponse,
+    StuffResponseDescription
+)
+
+admin.site.register(AccelerationProgram)
+admin.site.register(Stage)
+admin.site.register(JoinProgram)
+admin.site.register(Applicants)
+admin.site.register(StageType)
+admin.site.register(OrderedStages)
+admin.site.register(ApplicantResponse)
+admin.site.register(StuffResponseDescription)
