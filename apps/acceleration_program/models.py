411f4c2159c23410ac62d2aae2fb86677e0953f2
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-23 21:50:47 +0400

    add: model for acceleration program with appropriate fields

--- apps/acceleration_program/models.py
+++ apps/acceleration_program/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from apps.directions.models import Direction
+
+
+class AccelerationProgram(models.Model):
+    directions = models.ManyToManyField(Direction)
+    name = models.CharField(max_length=150)
+    requirements = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    program_start_time = models.DateField()
+    program_end_time = models.DateField()
+
+    registration_end_date = models.DateField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} - active={self.is_active}"
