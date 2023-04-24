39e8f992a7a9870e8c93500aa1a1f7e9b03d7363
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-24 12:05:30 +0400

    add: view which fetches directions by their id or list of them

--- apps/directions/views.py
+++ apps/directions/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
-# Create your views here.
+from apps.directions.models import Direction
+from apps.directions.serializers import DirectionsSerializer
+
+
+@extend_schema(tags=["Directions"])
+class DirectionsModelViewSet(ReadOnlyModelViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = DirectionsSerializer
+    lookup_field = 'id'
+    queryset = Direction.objects.all()
