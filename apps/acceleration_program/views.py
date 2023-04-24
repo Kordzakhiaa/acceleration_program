cc8afa86b105958d8998f5334893cfcf2ef84878
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-04-24 16:00:21 +0400

    add: registered applicants list view

--- apps/acceleration_program/views.py
+++ apps/acceleration_program/views.py
@@ -1,8 +1,12 @@
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
-from apps.acceleration_program.models import AccelerationProgram
-from apps.acceleration_program.serializers import AccelerationProgramSerializer
+from apps.acceleration_program.models import AccelerationProgram, Applicants
+from apps.acceleration_program.serializers import (
+    AccelerationProgramSerializer,
+    RegisteredApplicantsSerializer
+)
 from apps.accounts.permissions import IsStuffAccelerationOrAdminUser
 
 
@@ -11,3 +15,8 @@ class AccelerationProgramViewSet(ModelViewSet):
     serializer_class = AccelerationProgramSerializer
     lookup_field = 'id'
     queryset = AccelerationProgram.objects.all()
+
+
+class RegisteredApplicantsListAPIView(ListAPIView):
+    serializer_class = RegisteredApplicantsSerializer
+    queryset = Applicants.objects.all()
