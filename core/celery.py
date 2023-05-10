a82a5b884c7578f64b6a154bb6f713adb347bed7
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-05-11 00:11:01 +0400

    add: schedule that runs check_acceleration_program task at 00:01 every day

--- core/celery.py
+++ core/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
@@ -10,3 +11,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    # Executes every day at 00:01
+    'add-every-monday-morning': {
+        'task': 'apps.acceleration_program.tasks.check_acceleration_program',
+        'schedule': crontab(hour=0, minute=1),
+    },
+}
