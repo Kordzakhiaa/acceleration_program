a554d56c2e4b5956db85d61b732658f4acb0ffdb
Giorgi Kordzakhia <giorgi.kordzakhia@makingscience.com>
2023-05-14 15:02:36 +0400

    remove: static scheduler which was used for test purposes

--- core/celery.py
+++ core/celery.py
@@ -1,7 +1,6 @@
 import os
 
 from celery import Celery
-from celery.schedules import crontab
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
@@ -11,11 +10,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
-
-app.conf.beat_schedule = {
-    # Executes every day at 00:01
-    'check-every-day': {
-        'task': 'apps.acceleration_program.tasks.check_acceleration_program',
-        'schedule': crontab(hour=0),
-    },
-}
